957d88e89976cae438d2fb86cdb84c82490da8b1
Christian Rijke <crijke@gmail.com>
2013-05-15 15:14:53 +0200

    Remove reporting link on 'unit costs' in budget view. The view it linked to has been moved to the reporting plugin.
    - Also moved the code to a helper to be able to add a link by overriding the helper method.

--- app/helpers/cost_objects_helper.rb
+++ app/helpers/cost_objects_helper.rb
@@ -2,21 +2,20 @@ require 'csv'
 
 module CostObjectsHelper
   include ApplicationHelper
-    
+
   # Check if the current user is allowed to manage the budget.  Based on Role
   # permissions.
   def allowed_management?
     return User.current.allowed_to?(:edit_cost_objects, @project)
   end
-  
+
   def cost_objects_to_csv(cost_objects, project)
-    ic = Iconv.new(l(:general_csv_encoding), 'UTF-8')    
-    decimal_separator = l(:general_csv_decimal_separator)
+    ic = Iconv.new(l(:general_csv_encoding), 'UTF-8')
     export = StringIO.new
     CSV::Writer.generate(export, l(:general_csv_separator)) do |csv|
       # csv header fields
       headers = [ "#",
-                  l(:field_status), 
+                  l(:field_status),
                   l(:field_project),
                   l(:field_subject),
                   l(:field_author),
@@ -32,7 +31,7 @@ module CostObjectsHelper
       # csv lines
       cost_objects.each do |cost_object|
         fields = [cost_object.id,
-                  l(cost_object.status), 
+                  l(cost_object.status),
                   cost_object.project.name,
                   cost_object.subject,
                   cost_object.author.name,
@@ -40,7 +39,7 @@ module CostObjectsHelper
                   cost_object.kind == "VariableCostObject" ? number_to_currency(cost_object.material_budget) : "",
                   cost_object.kind == "VariableCostObject" ? number_to_currency(cost_object.labor_budget) : "",
                   cost_object.kind == "VariableCostObject" ? number_to_currency(cost_object.spent) : "",
-                  format_time(cost_object.created_on),  
+                  format_time(cost_object.created_on),
                   format_time(cost_object.updated_on),
                   cost_object.description
                   ]
@@ -49,5 +48,9 @@ module CostObjectsHelper
     end
     export.rewind
     export
+  end
+
+  def pluralized_unit_costs(cost_entry, issue)
+    pluralize(cost_entry.units, cost_entry.cost_type.unit, cost_entry.cost_type.unit_plural)
   end
 end
